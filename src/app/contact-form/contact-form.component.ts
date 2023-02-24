0ef1e3bb0d24af7bc9f512c05a6ff726114aa84f
Asad Ishfaq <asadishfaq@aquila360.com>
2023-02-24 16:22:24 +0500

    Template Driven form is completed

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent {
+  contactMethod = [
+    {
+      id: 1,
+      name: 'Email',
+    },
+    {
+      id: 2,
+      name: 'Phone',
+    },
+  ];
   onSubmit(data: any) {
     const { firstName, comment } = data.value;
     console.log(data.value);
